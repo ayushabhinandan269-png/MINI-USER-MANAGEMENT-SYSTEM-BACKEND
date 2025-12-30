16a2c98753f6c692f30f436065927d9590b4bf2b
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2025-12-31 02:11:01 +0530

    changed codes for app.js files

--- src/app.js
+++ src/app.js
@@ -6,15 +6,30 @@ import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+/* ---------- Middleware ---------- */
 app.use(express.json());
-app.use(errorHandler);
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mini-user-management-system-frontend-ha2f6nlmy.vercel.app"
+    ],
+    credentials: true
+  })
+);
+
+/* ---------- Routes ---------- */
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
+/* ---------- Health Check ---------- */
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+  res.send("Backend is running successfully 🚀");
 });
+
+/* ---------- Error Handler (LAST) ---------- */
+app.use(errorHandler);
 
 export default app;
 
